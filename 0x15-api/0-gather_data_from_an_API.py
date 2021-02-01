17fcc108d7317ce611e575863f3b65ceb8dac270
hachemhfaiedh <hechemhfaiedh@gmail.com>
2021-02-01 09:46:25 -0800

    data_fro;_api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Gather data from an API"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    U_ID = {'userId': argv[-1]}
+    ID = {'id': argv[-1]}
+    req1 = requests.get(
+        url="https://jsonplaceholder.typicode.com/todos",
+        params=U_ID)
+    req2 = requests.get(
+        url="https://jsonplaceholder.typicode.com/users",
+        params=ID)
+    todos = req1.json()
+    users = req2.json()
+
+    td = 0
+    for task in todos:
+        if task.get("completed"):
+            td += 1
+    name = users[0].get("name")
+    print("Employee {} is done with tasks({}/{}):".format(
+        name, td, len(todos)))
+    for task in todos:
+        if task.get("completed"):
+            print("\t {}".format(task.get("title")))

8b4bcc6f3908a070630a2fd542cf9e28b0269e4d
hachemhfaiedh <hechemhfaiedh@gmail.com>
2021-02-04 08:55:52 -0800

    How many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''number of subscribers for a given subreddit'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+    headers = {'User-agent': 'Hachem'}
+    try:
+        r = requests.get(
+            'https://www.reddit.com/r/{}/about.json'.format(subreddit),
+        headers=headers)
+        data = r.json()
+        return(data.get('data').get('subscribers'))
+    except AttributeError:
+        return 0
